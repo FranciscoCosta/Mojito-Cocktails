93a3c3999d2498bf8495dc17eb5006a9f2857292
Francisco Costa <francisco100eg@gmail.com>
2025-07-08 14:33:07 -0300

    Add Hero component with animations and update Navbar
    Introduces a new Hero component with GSAP animations, including text splitting and scroll-triggered effects, to enhance visual presentation. Updates Navbar background color for improved consistency. Enhances user experience with engaging animations and a clear layout.

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import Navbar from './components/navbar.jsx';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 import React from 'react'
+import Hero from './components/hero.jsx';
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className='h-dvh bg-black'>
+        </div>
     </main>
   )
 }
