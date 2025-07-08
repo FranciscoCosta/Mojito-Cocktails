2196f92e71e37d2c19c481066e67b2e5f0a6219f
Francisco Costa <francisco100eg@gmail.com>
2025-07-08 16:05:56 -0300

    Adds responsive video playback and media query handling
    Introduces a responsive video element with a timeline animation tied to scroll progress.
    
    Enhances mobile responsiveness by using media queries to adjust scroll trigger values dynamically. Utilizes `useRef` for video element manipulation and updates animations based on metadata loading.

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,11 +1,17 @@
 import { useGSAP } from '@gsap/react'
-import React from 'react'
+import React, {useRef} from 'react'
 import { SplitText } from 'gsap/SplitText'
 import gsap from 'gsap'
+import { useMediaQuery } from 'react-responsive'
 
 
 const Hero = () => {
 
+    const videoRef = useRef(null);
+
+    const isMobile = useMediaQuery({
+        query: '(maxWidth: 768px)'
+    });
     useGSAP(() => {
         const heroSplit = new SplitText('.title', {type: 'chars words'});
         const paragraphSplit = new SplitText('.subtitle', {type: 'lines'});
@@ -36,10 +42,29 @@ const Hero = () => {
                 scrub: true,
             }
         }).to('.left-leaf', {y : -200 }, 0)
-            .to('.right-leaf', {y : 200 }, 0)
+        .to('.right-leaf', {y : 200 }, 0)
+
+	const startValue = isMobile ? "top 50%" : "center 60%";
+	const endValue = isMobile ? "120% top" : "bottom top";
 
 
+        let tl = gsap.timeline({
+	 scrollTrigger: {
+		trigger: "video",
+		start: startValue,
+		end: endValue,
+		scrub: true,
+		pin: true,
+	 },
+	});
+	
+	videoRef.current.onloadedmetadata = () => {
+	 tl.to(videoRef.current, {
+		currentTime: videoRef.current.duration,
+	 });
+	}; 
 
+        
     }, []);
             
   return (
@@ -70,7 +95,10 @@ const Hero = () => {
                 </div>
             </div>
         </div>
-      </section>
+    </section>
+    <div className='video absolute inset-0'>
+        <video src='/videos/output.mp4' muted playsInline preload="auto" ref={videoRef}/>
+    </div>
    </>
   )
 }
