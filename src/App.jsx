478647fcce6226dc430510fd58ff70db5497adf2
Francisco Costa <francisco100eg@gmail.com>
2025-07-12 20:22:38 -0300

    Adds Cocktails component with parallax animations
    Introduces a new Cocktails component featuring parallax animations
    using GSAP and ScrollTrigger. Displays lists of popular cocktails
    and loved mocktails with dynamic data rendering. Integrates the
    component into the main application layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
+import React from 'react'
+import Hero from './components/hero.jsx';
+import Cocktails from './components/cocktails.jsx';
 import gsap from 'gsap';
 import {ScrollTrigger , SplitText} from 'gsap/all';
 import Navbar from './components/navbar.jsx';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
-import React from 'react'
-import Hero from './components/hero.jsx';
-
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
-      <div className='h-dvh bg-black'>
-        </div>
+      <Cocktails />
     </main>
   )
 }
